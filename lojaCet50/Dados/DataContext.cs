d0c3cbdfb9ee1786dae219b522d729c5f3154c25
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2020-11-18 17:34:36 +0000

    Criar a base de dados e tabela produtos.

---
+++ lojaCet50/Dados/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using lojaCet50.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace lojaCet50.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ lojaCet50/Dados/Entidades/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lojaCet50.Dados.Entidades
+{
+    public class Produto
+    {
+            public int Id { get; set; }
+
+            public string Nome { get; set; }
+
+
+            [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+            public decimal Preco { get; set; }
+
+
+            [Display(Name = "Image")]
+            public string UrlImagem { get; set; }
+
+
+            [Display(Name = "Última Compra")]
+            public DateTime UltimaCompra { get; set; }
+
+
+            [Display(Name = "Última Venda")]   
+            public DateTime UltimaVenda { get; set; }
+
+
+            [Display(Name = "Disponível")]
+            public bool Disponivel { get; set; }
+
+
+            [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+            public double Stock{ get; set; }
+    }
+}
